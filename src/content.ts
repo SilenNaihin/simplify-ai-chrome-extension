f64eb650a69a4581c17686c463bd5ee91ea0f6ee
Silen Naihin <silen.naihin@gmail.com>
2022-12-16 16:14:19 -0500

    highlight selection text

--- src/content.ts
+++ src/content.ts
@@ -1,5 +1,14 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'SIMPLIFY_GPT') {
     console.log(message.data);
+    const range = document?.getSelection()?.getRangeAt(0);
+    const span = document.createElement('span');
+    span.style.backgroundColor = 'yellow';
+    if (range) {
+      span.appendChild(range.extractContents());
+      range.insertNode(span);
+      //   range.deleteContents();
+      //   range.insertNode(document.createTextNode('newValue'));
+    }
   }
 });

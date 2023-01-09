33baf9be8442760f240721437d908768392bc5ee
Silen Naihin <silen.naihin@gmail.com>
2023-01-08 16:47:16 -0800

    still buggy but further fixes

--- src/content/highlightText.ts
+++ src/content/highlightText.ts
@@ -172,14 +172,14 @@ export const recursiveWrapper = ({
 
     // Step 2:
     // If we get here, we are in a text node, the start was found and we are not done highlighting
-    const { nodeValue }: any = element;
-    let parentNode: any = element.parentNode;
+    const { nodeValue } = element;
+    let parentElement = element.parentElement;
 
     console.log(
       'nodeValue',
       nodeValue,
       'parentNode',
-      parentNode,
+      parentElement,
       'startIndex',
       startIndex,
       'ancestor.childNodes',
@@ -188,7 +188,8 @@ export const recursiveWrapper = ({
 
     // if it's a whitespace text node
     if (ancestor.childNodes[x + 1]) {
-      parentNode = ancestor.childNodes[x + 1].parentNode;
+      // FIXME: do I need this? how do I handle just whitespaces that need to be highlighted
+      parentElement = ancestor.childNodes[x + 1].parentElement;
       console.log(
         'sometimes parentNode',
         '__________________________________',
@@ -198,78 +199,81 @@ export const recursiveWrapper = ({
       );
     }
 
-    if (startIndex > nodeValue.length) {
-      // Start index is beyond the length of the text node, can't find the highlight
-      // NOTE: we allow the start index to be equal to the length of the text node here just in case
-      throw new Error(
-        `No match found for highlight string '${selectionString}'`
-      );
-    }
-
-    // Split the text content into three parts, the part before the highlight, the highlight and the part after the highlight:
-    const textNode = element as Text;
-    const highlightTextEl = textNode.splitText(startIndex);
-
-    console.log('highlightTextEl', highlightTextEl, 'startIndex', startIndex);
-
-    // Instead of simply blindly highlighting the text by counting characters,
-    // we check if the text is the same as the selection string.
-    let i = startIndex;
-    for (; i < nodeValue.length; i++) {
-      // Skip any whitespace characters in the selection string as there can
-      // be more than in the text node:
-      while (
-        charsHighlighted < selectionLength &&
-        selectionString[charsHighlighted].match(/\s/u)
-      )
-        charsHighlighted++;
-
-      if (charsHighlighted >= selectionLength) break;
-
-      const char = nodeValue[i];
-      if (char === selectionString[charsHighlighted]) {
-        charsHighlighted++;
-      } else if (!char.match(/\s/u)) {
-        // FIXME: Here, this is where the issue happens
-        // Similarly, if the char in the text node is a whitespace, ignore any differences
-        // Otherwise, we can't find the highlight text; throw an error
+    // FIXME: sometimes its null. need to check why
+    if (nodeValue && parentElement) {
+      if (startIndex > nodeValue.length) {
+        // Start index is beyond the length of the text node, can't find the highlight
+        // NOTE: we allow the start index to be equal to the length of the text node here just in case
         throw new Error(
           `No match found for highlight string '${selectionString}'`
         );
       }
+
+      // Split the text content into three parts, the part before the highlight, the highlight and the part after the highlight:
+      const textNode = element as Text;
+      const highlightTextEl = textNode.splitText(startIndex);
+
+      console.log('highlightTextEl', highlightTextEl, 'startIndex', startIndex);
+
+      // Instead of simply blindly highlighting the text by counting characters,
+      // we check if the text is the same as the selection string.
+      let i = startIndex;
+      for (; i < nodeValue.length; i++) {
+        // Skip any whitespace characters in the selection string as there can
+        // be more than in the text node:
+        while (
+          charsHighlighted < selectionLength &&
+          selectionString[charsHighlighted].match(/\s/u)
+        )
+          charsHighlighted++;
+
+        if (charsHighlighted >= selectionLength) break;
+
+        const char = nodeValue[i];
+        if (char === selectionString[charsHighlighted]) {
+          charsHighlighted++;
+        } else if (!char.match(/\s/u)) {
+          // FIXME: Here, this is where the issue happens
+          // Similarly, if the char in the text node is a whitespace, ignore any differences
+          // Otherwise, we can't find the highlight text; throw an error
+          throw new Error(
+            `No match found for highlight string '${selectionString}'`
+          );
+        }
+      }
+
+      // If textElement is wrapped in a .highlighter--highlighted span, do not add this highlight
+      // as it is already highlighted, but still count the number of charsHighlighted
+      if (parentElement.classList.contains(HIGHLIGHT_CLASS)) return;
+
+      const elementCharCount = i - startIndex; // Number of chars to highlight in this particular element
+      const insertBeforeElement = highlightTextEl.splitText(elementCharCount);
+      const highlightText = highlightTextEl.nodeValue;
+
+      // If the text is all whitespace, ignore it
+      if (highlightText?.match(/^\s*$/u)) {
+        parentElement.normalize(); // Undo any 'splitText' operations
+        return;
+      }
+
+      console.log(
+        `${elementCharCount} chars highlighted in text node ${nodeValue}`
+      );
+
+      // If we get here, highlight!
+      // Wrap the highlighted text in a span with the highlight class name
+      const highlightNode = React.createElement(HighlightCard, {
+        phrase: highlightTextEl.nodeValue,
+        key: key,
+      });
+
+      const container = document.createElement('span');
+
+      ReactDOM.render(highlightNode, container);
+
+      highlightTextEl.remove();
+      parentElement.insertBefore(container, insertBeforeElement);
     }
-
-    // If textElement is wrapped in a .highlighter--highlighted span, do not add this highlight
-    // as it is already highlighted, but still count the number of charsHighlighted
-    if (parentNode.classList.contains(HIGHLIGHT_CLASS)) return;
-
-    const elementCharCount = i - startIndex; // Number of chars to highlight in this particular element
-    const insertBeforeElement = highlightTextEl.splitText(elementCharCount);
-    const highlightText = highlightTextEl.nodeValue;
-
-    // If the text is all whitespace, ignore it
-    if (highlightText?.match(/^\s*$/u)) {
-      parentNode.normalize(); // Undo any 'splitText' operations
-      return;
-    }
-
-    console.log(
-      `${elementCharCount} chars highlighted in text node ${nodeValue}`
-    );
-
-    // If we get here, highlight!
-    // Wrap the highlighted text in a span with the highlight class name
-    const highlightNode = React.createElement(HighlightCard, {
-      phrase: highlightTextEl.nodeValue,
-      key: key,
-    });
-
-    const container = document.createElement('span');
-
-    ReactDOM.render(highlightNode, container);
-
-    highlightTextEl.remove();
-    parentNode.insertBefore(container, insertBeforeElement);
   });
 
   return [startFound, charsHighlighted];
